3b41a4a6d76afc5fb3915c0f3e81bd28b08ebf57
Henry Qin <hq6@stanford.edu>
2017-02-20 20:52:55 -0800

    Changes to cluster scripts to separate clients from coordinators and masters

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -638,7 +638,7 @@ def run(
         coordinator = cluster.start_coordinator(coordinator_host,
                                                 coordinator_args)
         if disjunct:
-            cluster.hosts.pop(0)
+            cluster.hosts.pop()
 
         if old_master_host:
             oldMaster = cluster.start_server(old_master_host,
@@ -685,6 +685,8 @@ def run(
                     host_list = cluster.hosts[num_servers:]
                     if share_hosts:
                         host_list.extend(cluster.hosts[:num_servers])
+                    else: # Remove the coordinator from this list
+                        host_list.pop()
 
                 client_hosts = [host_list[i % len(host_list)]
                                 for i in range(num_clients)]
